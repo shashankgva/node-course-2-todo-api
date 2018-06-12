86c94be9f947905f05cee23362b572fd297a2384
Shashank <shashankgva@gmail.com>
2018-06-12 23:43:50 +0530

    ADD Get todos By ID

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -29,6 +30,24 @@ app.get('/todos', (req, res) => {
 		res.status(400).send(e);
 	});
 })
+
+//GET todos/12345
+
+app.get('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		res.status(404).send({});
+	}
+
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			res.status(404).send({})
+		}
+		res.status(200).send({todo});
+	}).catch((e) => {
+		res.status(400).send(e);
+	});
+});
 
 
 
