1876eecff2d45f8f8ba919f5135f2345b1025e57
Shashank <shashankgva@gmail.com>
2018-06-13 23:48:48 +0530

    Set up app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 var connect_str;
 
 if (process.env.PORT) {
-	connect_str = 'mongodb://ds159100.mlab.com:59100/todoapp';
+	connect_str = 'mongodb://shashankgva:abc123@ds159100.mlab.com:59100/todoapp';
 } else {
 	connect_str = 'mongodb://localhost:27017/TodoApp';
 }
