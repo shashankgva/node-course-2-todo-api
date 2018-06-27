e14b8a55b155cb1d6acb9e0cd935a71e191c7c8c
Shashank <shashankgva@gmail.com>
2018-06-27 20:42:53 +0530

    Add get users me and auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,26 @@ UserSchema.methods.generateAuthToken = function () {
 
 };
 
+UserSchema.statics.findByToken = function (token) {
+	let User = this;
+	let decoded;
+
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch (e) {
+		// return new Promise(resolve, reject) => {
+		// 	reject('Rejected');
+		// };
+		return Promise.reject('Rejected');
+	}
+
+	return User.findOne({
+		'_id' : decoded._id,
+		'tokens.token': token,
+		'tokens.access' : 'auth'
+	});
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
